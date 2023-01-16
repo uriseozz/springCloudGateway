c2ff8783ccd4f53528758ec2ad6269273f3a5feb
YuriSeo <iyoule@naver.com>
2023-01-16 15:20:01 +0900

    설정변경

--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping("/user")
+    @GetMapping("/user/response")
     public String get() {
         return "User MSA Response";
     }
